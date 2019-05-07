d0ef05e726e0e1cb015c69e8f70a0294237eedac
dexterp23 <ivandexter26@yahoo.com>
2019-05-07 11:37:55 +0200

    message

--- www/js/functions_audioloop.js
+++ www/js/functions_audioloop.js
@@ -4,9 +4,15 @@ function WelcomePage () {
 	nextPage('WelcomePage');
 	$.ui.clearHistory();
 	
-	global_audio_data[0] = "/audio_loop.mp3";
-	global_audio_data[1] = "/audio_1.mp3";
-	global_audio_data[2] = "/audio_2.mp3";
+	global_audio_data[0] = "audio_loop.mp3";
+	global_audio_data[1] = "audio_1.mp3";
+	global_audio_data[2] = "audio_2.mp3";
+	
+	/*
+	global_audio_data[0] = global_host+"/audio_loop.mp3";
+	global_audio_data[1] = global_host+"/audio_1.mp3";
+	global_audio_data[2] = global_host+"/audio_2.mp3";
+	*/
 
 	
 }
